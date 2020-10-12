1e5a16711cc1b6ab8c8dd563a1e220c246486d27
Santa-Garrick <58153357+Santa-Garrick@users.noreply.github.com>
2020-10-12 13:58:45 +0300

    Create 897-IncreasingOrderSearchTree.swift

---
+++ 897-IncreasingOrderSearchTree.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func increasingBST(_ root: TreeNode?) -> TreeNode? {        
+        var newRoot: TreeNode?
+        traverse(root, &newRoot)
+        return newRoot
+    }
+    
+    private func traverse(_ node: TreeNode?, _ newRoot: inout TreeNode?) {
+        guard let node = node else { return }
+        
+        
+        // reversed in-order
+        traverse(node.right, &newRoot)
+        
+        let newNode = TreeNode(node.val, nil, newRoot)
+        newRoot = newNode
+        
+        traverse(node.left, &newRoot)
+        
+    }
+}
